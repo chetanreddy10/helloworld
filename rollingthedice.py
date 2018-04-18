33d5f5bb1da37ff69637e0148f4cc7cf4f9fee70
chetanreddy10 <38429188+chetanreddy10@users.noreply.github.com>
2018-04-18 19:08:05 -0400

    Update rollingthedice.py

--- rollingthedice.py
+++ rollingthedice.py
@@ -2,9 +2,6 @@ import random
 min = 1
 max = 6
 
-roll_again = "yes"
-
-while roll_again == "yes" or roll_again == "y":
     print "Rolling the dices..."
     print "The values are...."
     print random.randint(min, max)
