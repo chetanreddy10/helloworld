0d028c1eb270a61b9fd46329cb3590335bf5d540
chetanreddy10 <38429188+chetanreddy10@users.noreply.github.com>
2018-04-18 19:09:22 -0400

    Update rollingthedice.py

--- rollingthedice.py
+++ rollingthedice.py
@@ -1,10 +1,9 @@
 import random
 min = 1
 max = 6
-
-    print "Rolling the dices..."
-    print "The values are...."
-    print random.randint(min, max)
-    print random.randint(min, max)
+print "Rolling the dices..."
+print "The values are...."
+print random.randint(min, max)
+print random.randint(min, max)
 
     
