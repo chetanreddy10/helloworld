e6fa6cd2df5229652576ec01d8f39b7fa5dd2495
chetanreddy10 <38429188+chetanreddy10@users.noreply.github.com>
2018-04-29 16:18:17 -0400

    Create average.py

---
+++ average.py
@@ -0,0 +1,12 @@
+# Get three test score
+round1 = 20
+
+round2 = 50
+
+round3 = 70
+   
+# Calculate the average
+average = (round1 + round2 + round3) / 3
+
+# Print out the test score
+print "the average score is: ", average 
