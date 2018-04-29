e5df442ba46f86e6aa032dfda0da23bb118fe0cd
chetanreddy10 <38429188+chetanreddy10@users.noreply.github.com>
2018-04-29 16:44:41 -0400

    Update average.py
    4th average

--- average.py
+++ average.py
@@ -4,9 +4,11 @@ round1 = 20
 round2 = 50
 
 round3 = 70
+
+round4 = 100
    
 # Calculate the average
-average = (round1 + round2 + round3) / 3
+average = (round1 + round2 + round3 + round4) / 4
 
 # Print out the test score
 print "the average score is: ", average 
